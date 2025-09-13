a5b42ab751f6c53ef90d9601c4fa41bfa19e258b
SinghaAnirban005 <anirbansingha20@gmail.com>
2025-09-14 00:55:06 +0530

    Add login api
    Signed-off-by: SinghaAnirban005 <anirbansingha20@gmail.com>

--- apps/api/Controllers/Auth.ts
+++ apps/api/Controllers/Auth.ts
@@ -4,7 +4,59 @@ import bcrypt from "bcrypt"
 import jwt from "jsonwebtoken"
 import { prismaClient } from "prisma/client"
  
-app.post("/api/v1/login", (req, res) => {
+app.post("/api/v1/login", async(req: Request, res: Response) => {
+    const { username , password } = req.body();
+
+    if(!username || password){
+        res.status(401).json({
+            message: "Username or Password is missing"
+        })
+    }
+
+    const user = await prismaClient.user.findFirst({
+        where: {
+            username: username
+        }
+    })
+
+    if(!user){
+        res.status(401).json({
+            message: "No user exists"
+        })
+
+        return;
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(user.password, password)
+
+    if(!isPasswordCorrect){
+        res.status(401).json({
+            message: "Password is incorrect"
+        })
+        
+        return;
+    }
+
+    const genToken = await jwt.sign({
+        username: username
+    }, process.env.JWT_SECRET)
+
+    if(!genToken){
+        res.status(401).json({
+            message: "Token gen failed"
+        })
+
+        return;
+    }
+
+    res.status(200).json({
+        message: {
+            username: user.username;
+            token: genToken
+        }
+    })
+
+    return;
 
 })
 
