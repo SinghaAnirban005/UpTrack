0e18704ea7d1dbe70899989f2acbdbdbe682b1b2
SinghaAnirban005 <anirbansingha20@gmail.com>
2025-09-30 01:42:03 +0530

    pkg: Add redis utility methods
    Signed-off-by: SinghaAnirban005 <anirbansingha20@gmail.com>

---
+++ packages/redis-stream/index.ts
@@ -0,0 +1,62 @@
+import { createClient } from "redis"
+
+
+const client = createClient()
+                .on("error", (err) => {
+                    console.log(err)
+                })
+                .connect()
+
+type WebsiteEvent = {
+        url: string
+        id: string,
+}
+
+type MessageType = {
+    id: string,
+    message: {
+        id: string,
+        url: string
+    }
+}
+
+const STREAM_NAME = "UpTrack:website"
+
+async function xAdd({url, id}: WebsiteEvent){
+    (await client).xAdd(
+        STREAM_NAME, '*', {
+            url,
+            id
+        }
+    )
+}
+
+export const xAddBulk = async(websites: WebsiteEvent[]) => {
+    for(let i = 0; i < websites.length; i++){
+        await xAdd({
+            url: websites[i]?.url as string,
+            id: websites[i]?.id as string
+        })
+    }
+}
+
+export async function xReadGroup(consumerGroup: string, workerId: string){
+    const res = (await client).xReadGroup(consumerGroup, workerId, {
+        key: STREAM_NAME,
+        id: '>'
+    }, {
+        COUNT: 5
+    })
+    //@ts-ignore
+    let  messages: MessageType | undefined = res?.[0]?.messages;
+
+    return messages
+}
+
+async function xAck(consumerGroup: string, eventId: string){
+    (await client).xAck(STREAM_NAME, consumerGroup, eventId)
+}
+
+export const xAckBulk = async(consumerGroup: string, eventIds: string[]) => {
+    eventIds.map(event => xAck(consumerGroup, event))
+}
